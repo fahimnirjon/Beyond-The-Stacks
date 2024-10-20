1a44e54f45f4d71815be9fe562fe1347a979eac5
fahimnirjon <fahimnirjon21@gmail.com>
2024-10-21 01:33:41 +0600

    added

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://beyond-the-stacks.vercel.app/p', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'fahimnirjon21@gmail.com', 
@@ -14,7 +14,7 @@ const siteMetadata = {
     twitter: 'https://x.com/Fahim_Nirjon04',
     facebook: 'https://www.facebook.com/fahimshahriar.nirjon/',
     linkedin: 'https://www.linkedin.com/in/fahimnirjon04/',
-    dribbble: 'https://www.dribbble.com',
+    dribbble: 'https://fahimnirjon.github.io/Fahim-Nirjon/',
     locale: 'Dhaka, Bangladesh',
   }
   
