64de69bdb00aab7192f5694bc4142a46e60665cb
fahimnirjon <fahimnirjon21@gmail.com>
2024-10-21 01:25:16 +0600

    first commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,21 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog Website With Tailwind CSS and Contentlayer',
+    author: 'Fahim Nirjon',
+    headerTitle: 'Beyond The Stack',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'fahimnirjon21@gmail.com', 
+    github: 'https://github.com/fahimnirjon',
+    twitter: 'https://x.com/Fahim_Nirjon04',
+    facebook: 'https://www.facebook.com/fahimshahriar.nirjon/',
+    linkedin: 'https://www.linkedin.com/in/fahimnirjon04/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'Dhaka, Bangladesh',
+  }
+  
+  module.exports = siteMetadata
